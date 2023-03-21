9a7b59e64f56cb226bcf546059b28125ab0deba3
VadEgo <vadimegorov@outlook.com>
2023-03-21 15:46:27 +1100

    removed title

--- force-app/main/default/lwc/homePageComponent/homePageComponent.js
+++ force-app/main/default/lwc/homePageComponent/homePageComponent.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class MiscNotification extends LightningElement {
-    _title = 'Sample Title';
+    _title = 'My Sample Title';
     message = 'Sample Message';
     variant = 'error';
     variantOptions = [
